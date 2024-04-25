b6ea90721547ffea376d5fd07e5870a7a98a6df6
RifqiRasyad <rifqi.rasyad@gmail.com>
2024-04-25 23:54:08 +0700

    features2

--- js/script.js
+++ js/script.js
@@ -18,11 +18,32 @@ function reverse() {
 }
 
 function convert() {
-    let celcius = document.getElementById('c-to-f-input').ariaValueMax;
-    if (celcius.match(/[1-4]/g)) {
-        alert("tolong input berupa angka saja!")
+    let inputNilai = document.getElementBy ('c-to-f-input')[0].id;
+    if (inputNilai === "input1") {
+        let Celcius = parseFloat(document.getElementById("input1").value);
+        if(!isNaN(Celcius)) {
+            let pilih = (Celcius * 9/5);
+            let fahrenheit = pilih + 32;
+            document.getElementById("input2").value = fahrenheit.toFixed(2);
+
+            let resultText = "Fahrenheit = (Celcius x 9/5) + 32\r\n= (" + Celcius + " x 9/5) + 32"
+            document.getElementById("result").textContent = resultText;
+        } else {
+            document.getElementById("result").textContent = "Tolong isi Suhu dahulu";
+        }
+    } else if (inputNilai === "input2") {
+        let fahrenheit = parseFloat(document.getElementById("input2").value);
+        if (isNaN(fahrenheit)) {
+            let pilih = fahrenheit + 32;
+            let Celcius = pilih * 5/9;
+            document.getElementById("input1").value = Celcius.toFixed(2);
+
+            let resultText = "C = (F - 32) x 5/9\r\n= (" + fahrenheit + "-32) *5/9";
+            document.getElementById("result").textContent = resultText;
+        } else {
+            document.getElementById("result").textContent = "Tolong isi Suhu dahulu";
+        }
     }
-    console.log(celcius);
 }
 
 document.getElementById("button-convert").addEventListener('click', () => convert());
